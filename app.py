84c004ccdfed6311383e79d1e1394f7c27eb27e7
t0m0h1 <thomashathawayhogg@gmail.com>
2025-01-09 01:51:41 +0000

    1st working version tested flask

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def preprocess_text(text):
 # Routes
 @app.route('/')
 def index():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
