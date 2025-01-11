b54b52b87607fadf9811464c744d6491055a1d52
t0m0h1 <thomashathawayhogg@gmail.com>
2025-01-11 21:32:00 +0000

    training.py

--- training.py
+++ training.py
@@ -13,3 +13,19 @@ from tensorflow.keras.optimizers import SGD
 lemmatiser = WordNetLemmatizer()
 
 intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['!', '?', ',', '.']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+
+print(documents)
