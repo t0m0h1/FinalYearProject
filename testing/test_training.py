64e9edcc2de9913704c278cb409b04b56dd59fe3
t0m0h1 <thomashathawayhogg@gmail.com>
2025-02-20 20:07:41 +0000

    Add test case boilerplate for training.py

---
+++ testing/test_training.py
@@ -0,0 +1,17 @@
+import unittest
+from unittest.mock import patch, mock_open
+import json
+import numpy as np
+import os
+import pickle
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from training import lemmatiser, intents, words, classes, training, model
+
+class TestChatbotTraining(unittest.TestCase):
+    pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
